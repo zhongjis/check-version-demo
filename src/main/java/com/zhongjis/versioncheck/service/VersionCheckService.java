1c5aebbf7732844e88f560a3b02f105990f8c6d6
Zhongjie Shen <zhongjis@uci.edu>
2020-09-24 21:48:46 -0700

    [feaure/WIP] modified VersionCheckService.java add checking algorithm

--- src/main/java/com/zhongjis/versioncheck/service/VersionCheckService.java
+++ src/main/java/com/zhongjis/versioncheck/service/VersionCheckService.java
@@ -6,11 +6,36 @@ import org.springframework.stereotype.Service;
 public class VersionCheckService {
 
   public String checkVersion(String versionOne, String versionTwo) {
-    String result = "?";
     // check if both Strings are legal
 
     // compare
+    String[] splitedOne = versionOne.split("\\.");
+    String[] splitedTwo = versionTwo.split("\\.");
 
-    return result;
+    return compareTwoVersionArr(splitedOne, splitedTwo);
+  }
+
+  private String compareTwoVersionArr(String[] splitedOne, String[] splitedTwo) {
+    int lenOne = splitedOne.length;
+    int lenTwo = splitedTwo.length;
+
+    int index = 0;
+
+    while (index < lenOne && index < lenTwo) {
+      int numOne = Integer.parseInt(splitedOne[index]);
+      int numTwo = Integer.parseInt(splitedTwo[index]);
+
+      if (numOne > numTwo) return ">";
+      else if (numOne < numTwo) return "<";
+      index++;
+    }
+
+    if (lenOne > lenTwo) {
+      return ">";
+    } else if (lenOne < lenTwo) {
+      return "<";
+    } else {
+      return "=";
+    }
   }
 }
