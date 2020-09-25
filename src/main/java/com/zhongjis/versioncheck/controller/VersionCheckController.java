e0bbe05c56910bb987fe9a6f1c85a5d446c6854d
Zhongjie Shen <zhongjis@uci.edu>
2020-09-24 21:26:24 -0700

    [feature/WIP] add VersionCheckController and VersionCheckService Service algorithm is not finished. api/v1/version/helloworld api/v1/version/check

---
+++ src/main/java/com/zhongjis/versioncheck/controller/VersionCheckController.java
@@ -0,0 +1,48 @@
+package com.zhongjis.versioncheck.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhongjis.versioncheck.service.VersionCheckService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/version")
+public class VersionCheckController {
+
+  private final VersionCheckService versionCheckService;
+
+  @Autowired
+  public VersionCheckController(VersionCheckService versionCheckService) {
+    this.versionCheckService = versionCheckService;
+  }
+
+  @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+  public ResponseEntity<String> greeting() {
+    return new ResponseEntity<>("Hello World from Zhongjie Shen", HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/check", method = RequestMethod.POST)
+  public ResponseEntity<String> checkVersion(@RequestBody String versionsList)
+      throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode = objectMapper.readTree(versionsList);
+
+    String firstVersionId = jsonNode.get("firstVersionId").asText();
+    String secondVersionId = jsonNode.get("secondVersionId").asText();
+
+    String symbol = versionCheckService.checkVersion(firstVersionId, secondVersionId);
+
+    String result = firstVersionId + " " + symbol + " " + secondVersionId;
+
+    return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+
+}
---
+++ src/main/java/com/zhongjis/versioncheck/service/VersionCheckService.java
@@ -0,0 +1,16 @@
+package com.zhongjis.versioncheck.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VersionCheckService {
+
+  public String checkVersion(String versionOne, String versionTwo) {
+    String result = "?";
+    // check if both Strings are legal
+
+    // compare
+
+    return result;
+  }
+}
