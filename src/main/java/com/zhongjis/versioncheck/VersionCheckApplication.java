a4bd5ceeacb93f3e83978f6192c9270a0f376bc0
Zhongjie Shen <zhongjis@uci.edu>
2020-09-24 20:47:02 -0700

    [init] first commit

---
+++ src/main/java/com/zhongjis/versioncheck/VersionCheckApplication.java
@@ -0,0 +1,13 @@
+package com.zhongjis.versioncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionCheckApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VersionCheckApplication.class, args);
+  }
+
+}
