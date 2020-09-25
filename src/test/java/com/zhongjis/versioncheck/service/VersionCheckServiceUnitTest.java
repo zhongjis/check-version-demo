9a9ea56f7e8aa28a49f4b230983dd0190a4b594c
Zhongjie Shen <zhongjis@uci.edu>
2020-09-24 23:41:30 -0700

    [test] add unit tests for CheckVersionService

---
+++ src/test/java/com/zhongjis/versioncheck/service/VersionCheckServiceUnitTest.java
@@ -0,0 +1,35 @@
+package com.zhongjis.versioncheck.service;
+
+import static org.junit.Assert.assertEquals;
+
+import com.zhongjis.versioncheck.controller.exception.IlegalVersionIdException;
+import com.zhongjis.versioncheck.service.impl.VersionCheckServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VersionCheckServiceUnitTest {
+
+  @InjectMocks
+  VersionCheckServiceImpl versionCheckService;
+
+  @Test
+  public void testCheckVersionWithLegalVersionId() throws IlegalVersionIdException{
+    assertEquals("1.0.0 is \"before\" 2.0.0", versionCheckService.checkVersion(
+        "1.0.0", "2.0.0"), "1.0.0 is \"before\" 2.0.0");
+    assertEquals("1.0 is \"before\" 2.0.0", versionCheckService.checkVersion(
+        "1.0", "2.0.0"), "1.0 is \"before\" 2.0.0");
+    assertEquals("2.0.0 is \"after\" 1.0.0", versionCheckService.checkVersion(
+        "2.0.0", "1.0.0"), "2.0.0 is \"after\" 1.0.0");
+    assertEquals("2.0 is \"before\" 3.0.0", versionCheckService.checkVersion(
+        "2.0", "3.0.0"), "2.0 is \"before\" 3.0.0");
+  }
+
+  @Test(expected = IlegalVersionIdException.class)
+  public void testCheckVersionWithIlegalVersionId() throws IlegalVersionIdException{
+    versionCheckService.checkVersion("a.0.0", "1.1.1");
+    versionCheckService.checkVersion("1.0.0", "c.1.1");
+  }
+}
